533390ce656bcb9558e3c8897d68b01231d91c09
Mister Souza <mistersouza@icloud.com>
2023-04-13 09:37:31 +0200

    Add About compoment to submit page

--- src/pages/r/[community]/submit.tsx
+++ src/pages/r/[community]/submit.tsx
@@ -9,24 +9,26 @@ import { auth } from '@/firebase/client'
 
 import { useSelector } from 'react-redux'
 import { RootState } from '@/app/store'
+import About from '@/pages/components/community/About'
 
 const Submit = () => {
   const [ user ] = useAuthState(auth)
-  const { currentCommunity } = useSelector((state: RootState) => state.communityPage);
+  const { currentCommunity: data } = useSelector((state: RootState) => state.communityPage);
   
   
   return (
     <PageLayout>
       <>
-        <div className='border-b border-white py-3.5'>
+        <div className='border-b border-white py-3.5 pr-3 flex items-center justify-between'>
           <p>Create a post</p>
+          <p className='text-[0.6rem] uppercase font-semibold text-blue-500'>Drafts<span className='w-1 h-1 ml-1 p-0.5 bg-gray-500 text-white'>0</span></p>
         </div>
-        { user && <PostForm user={user} /> }
+        {user && <PostForm user={user} />}
       </>
       <>
-        <div>
-          {/* <CommunitySidebar /> */}
-        </div>
+        <>
+          {data && <About community={data} />}
+        </>
       </>
     </PageLayout>
   )
