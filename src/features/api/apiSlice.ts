e245cd76da126c92cda4c47e08758c1a8f081c82
Mister Souza <mistersouza@icloud.com>
2023-04-18 09:39:21 +0200

    Implement temp fix to query

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -1,10 +1,11 @@
 // Importing the createApi and fakeBaseQuery from React-specific entry point
 import { createApi, fakeBaseQuery } from '@reduxjs/toolkit/query/react';
 import { User } from 'firebase/auth';
-import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, orderBy, query, setDoc, updateDoc, where } from 'firebase/firestore';
+import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, increment, orderBy, query, setDoc, updateDoc, where, writeBatch } from 'firebase/firestore';
 import { deleteObject, getDownloadURL, ref, uploadString } from 'firebase/storage';
 
 import { firestore, storage } from '../../../src/firebase/client';
+import { Comment } from '../commentsSlice';
 import { Post, Vote } from '../postsSlice';
 
 // Define a single API slice object
@@ -12,7 +13,7 @@ export const apiSlice = createApi({
     // the cache reducer expects to be added at 'state.api' (already default - this is optional)
     reducerPath: 'api',
     baseQuery: fakeBaseQuery(),
-    tagTypes: ['Post', 'CommunitySnippet', 'Vote'],
+    tagTypes: ['Post', 'CommunitySnippet', 'Vote', 'Comment'],
     endpoints: builder => ({
         fetchCommunitySnippets: builder.query({
             queryFn: async(user) => {
@@ -30,14 +31,31 @@ export const apiSlice = createApi({
             providesTags: ['CommunitySnippet']
         }),
         fetchCommunityPosts: builder.query({
-            queryFn: async(name) => {
+            queryFn: async(communityName) => {
                 try {
-                    const postsQuery = query(collection(firestore, 'posts'), where('communityName', '==', name), orderBy('createdAt', 'desc'));
+                    const postsQuery = query(collection(firestore, 'posts'), where('communityName', '==', communityName), orderBy('createdAt', 'desc'));
                     const querySnapShot = await getDocs(postsQuery);
 
                     return { data: querySnapShot.docs.map(doc => ({ ...doc.data() }))};
                 } catch (error: any) {
                     return { data: error }; 
+                }
+            },
+            providesTags: ['Post']
+        }),
+        fetchPost: builder.query({
+            queryFn: async(postId) => {
+                try {
+                    const postRef = doc(firestore, 'posts', postId);   
+                    const postSnapShot = await getDoc(postRef);
+
+                    if (postSnapShot.exists()) {
+                        return { data: { ...postSnapShot.data() } };
+                    } else {
+                        return { data: null };
+                    }
+                } catch (error: any) {
+                    return { data: error };
                 }
             },
             providesTags: ['Post']
@@ -70,6 +88,22 @@ export const apiSlice = createApi({
                 }
             },
             invalidatesTags: ['Vote']
+        }),
+
+        fetchCommunityVotes: builder.query({
+            queryFn: async(user, communityName) => {
+                try {
+
+                    // where('communityName', '==', communityName) does not work
+                    const votesQuery = query(collection(firestore, 'users', `${user?.uid}/votes`));
+                    const querySnapShot = await getDocs(votesQuery);
+
+                    return { data: querySnapShot.docs.map(doc => ({ Id: doc.id, ...doc.data() }))};
+                } catch (error: any) {
+                    return { data: error };
+                }
+            },
+            providesTags: ['Vote']
         }),
 
         deletePost: builder.mutation({
@@ -126,9 +160,63 @@ export const apiSlice = createApi({
             },
             invalidatesTags: ['Post']
         }),
-            
+        createComment: builder.mutation({
+            queryFn: async(comment: Comment) => {
+                try {
+                    const batch = writeBatch(firestore);
+
+                    const commentRef = doc(collection(firestore, 'comments'));
+
+                    batch.set(commentRef, { ...comment, id: commentRef.id });
+
+                    const postRef = doc(firestore, 'posts', comment.postId!);
+                    batch.update(postRef, { numberOfComments: increment(1) });
+
+                    await batch.commit();
+
+                    return { data: { commentRef } };
+                } catch (error) {
+                    return { data: null };
+                }
+            },
+            invalidatesTags: ['Comment']
+        }),
+        deleteComment: builder.mutation({
+            queryFn: async(commentId: string) => {
+                try {
+                    const commentRef = doc(firestore, 'comments', commentId!);
+                    const commentSnapShot = await getDoc(commentRef);
+
+                    if (commentSnapShot.exists()) {
+                        await deleteDoc(commentRef);
+                        return { data: { commentId } };
+                    } 
+
+                    return { data: { commentId } };
+                } catch (error) {
+                    return { data: null };
+                }
+            },
+            invalidatesTags: ['Comment']
+        }),
+        fetchComments: builder.query({  
+            queryFn: async(post) => {
+                // func should take postId, and query(collection(firestore, 'comments'), orderBy('createdAt', 'desc')) should
+                try {
+                    const commentsQuery = query(collection(firestore, 'comments'));
+                    const querySnapShot = await getDocs(commentsQuery);
+                    const comments = querySnapShot.docs.map(doc => ({ ...doc.data() }));
+                    
+                    // temp fix
+                    return { data: comments.filter((comment) => comment.postId === post.id) };
+                } catch (error) {
+                    return { data: null };
+                }
+            },
+            providesTags: ['Comment']
+        }),
     })
 });
 
-export const { useFetchCommunityPostsQuery, useFetchCommunitySnippetsQuery, useDeletePostMutation, useCreatePostMutation, useUploadImageMutation, useVoteMutation } = apiSlice;
+export const { useFetchCommunityPostsQuery, useFetchCommunitySnippetsQuery, useFetchCommunityVotesQuery, useDeletePostMutation, useCreatePostMutation, useUploadImageMutation, useVoteMutation, useFetchPostQuery, useCreateCommentMutation, useDeleteCommentMutation, useFetchCommentsQuery } = apiSlice;
 
