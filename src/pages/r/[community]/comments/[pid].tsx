109f85b2f8b3f225e28143f6844c95967ba94080
Mister Souza <mistersouza@icloud.com>
2023-04-20 18:00:08 +0200

    Set active post on re-render

--- src/pages/r/[community]/comments/[pid].tsx
+++ src/pages/r/[community]/comments/[pid].tsx
@@ -8,26 +8,32 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/app/store';
 
 import { useFetchPostQuery } from '@/features/api/apiSlice';
-
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '@/firebase/client';
-import PostHeader from '@/pages/components/Post/PostHeader';
-import { useRouter } from 'next/router';
 import { Post as PostType, setPost } from '@/features/postsSlice';
 import About from '@/pages/components/community/About';
 import Moderators from '@/pages/components/community/Moderators';
 import Comments from '@/pages/components/Post/comments';
 import { User } from 'firebase/auth';
+import { useRouter } from 'next/router';
 
 const PostPage = () => {
     const [ user ] = useAuthState(auth)
     const { post, votes } = useSelector((state: RootState) => state.posts);
     const { currentCommunity: data } = useSelector((state: RootState) => state.communityPage);
-    // page doesn't not load if you refresh the page. Post is null
+
+    
+    const router = useRouter();
+    const dispatch = useDispatch();
+    
+    // rendering the post page with the post id
+    const { pid } = router.query;
+    const { data: postData } = useFetchPostQuery(pid as string)
+
+    if (!post) dispatch(setPost(postData))
 
   return (
         <div className='w-full'>
-            <PostHeader />
             <PageLayout>
                 <>
                     {post && <Post post={post} isUserPost={post.authorId === user?.uid} onVote={() => {}} voteScore={votes.find(vote => vote.postId === post.id)?.value} deletePost={() => {}} />}
