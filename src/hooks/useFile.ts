9330fc29b8bce1b88ac26e19d6cb56b2faef2717
Mister Souza <mistersouza@icloud.com>
2023-04-21 17:28:06 +0200

    Implement useFile custom hook

---
+++ src/hooks/useFile.ts
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+const useFile = () => {
+    const [ file, setFile ] = useState<string | undefined>(undefined);
+    
+    const handleFileChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+        if (target.files?.[0]) {
+            const reader = new FileReader();
+            reader.readAsDataURL(target.files?.[0]);
+
+            reader.onload = () => {
+                if (reader.result) {
+                setFile(reader.result as string);
+                }
+            }
+        }
+    }
+
+    return { file, handleFileChange, setFile }; 
+}
+
+export default useFile
